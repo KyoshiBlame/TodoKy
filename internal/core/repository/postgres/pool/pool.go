dcc0840ddd6dc246da646615fec460750c65875a
kyoshi <give_me_gg@mail.ru>
2026-07-03 01:16:48 +0700

    feat Config postgres repository

---
+++ internal/core/repository/postgres/pool/pool.go
@@ -0,0 +1,20 @@
+package core_postgres_pool
+
+import "github.com/jackc/pgx/v5/pgxpool"
+
+
+
+
+
+type ConnectionPool struct {
+	*pgxpool.Pool
+}
+
+func NewConnectionPool() *ConnectionPool {
+
+	connectionString := 
+
+	return *ConnectionPool {
+
+	}
+}
