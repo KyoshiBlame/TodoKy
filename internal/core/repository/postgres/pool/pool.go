0316745c33cbe523fea107494e2a86326e46c757
kyoshi <give_me_gg@mail.ru>
2026-07-09 16:37:37 +0700

    fix: repository postgres pool

--- internal/core/repository/postgres/pool/pool.go
+++ internal/core/repository/postgres/pool/pool.go
@@ -1,20 +1,51 @@
 package core_postgres_pool
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"fmt"
+	"time"
 
-
-
-
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type ConnectionPool struct {
 	*pgxpool.Pool
+	opTimeout time.Duration
 }
 
-func NewConnectionPool() *ConnectionPool {
+func NewConnectionPool(
+	ctx context.Context,
+	config Config,
+) (*ConnectionPool, error) {
 
-	connectionString := 
+	connectionString := fmt.Sprintf(
+		"postgres://%s:%s@%s/%s?sslmode=disable",
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.Database,
+	)
 
-	return *ConnectionPool {
+	pgxConfig, err := pgxpool.ParseConfig(connectionString)
 
+	if err != nil {
+		return nil, fmt.Errorf("parse pgxConfig: %w", err)
 	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgxConfig)
+
+	if err != nil {
+		return nil, fmt.Errorf("create new pgx pool: %w", err)
+	}
+
+	if err := pool.Ping(); err != nil {
+		return nil, fmt.Errorf("pgxPool ping: %w", err)
+	}
+
+	return &ConnectionPool{Pool: pool, OpTimeout: config.Timeout}, nil
+}
+
+func (p *ConnectionPool) OpTimeout() time.Duration {
+	return p.opTimeout
 }
